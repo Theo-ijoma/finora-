acdde56f94bda5f9f9c545ee739c53e1ee151476
Theodore <ijomatheodore@gmail.com>
2025-07-02 17:21:45 +0100

    auth

--- constants/authContext.ts
+++ constants/authContext.ts
@@ -15,72 +15,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
 }) => {
   const [user, setUser] = useState<UserType | null>(null);
 
-  const login = async (email: string, password: string) => {
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-      return { success: true };
-    } catch (error: any) {
-      return { success: false, msg: error.message };
-    }
-  };
-
-  const register = async (email: string, password: string, name: string) => {
-    try {
-      const response = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-
-      await setDoc(doc(firestore, "users", response.user.uid), {
-        name,
-        email,
-        uid: response.user.uid,
-      });
-
-      return { success: true };
-    } catch (error: any) {
-      return { success: false, msg: error.message };
-    }
-  };
-
-  const updateUserData = async (uid: string) => {
-    try {
-      const docRef = doc(firestore, "users", uid);
-      const docSnap = await getDoc(docRef);
-
-      if (docSnap.exists()) {
-        const data = docSnap.data();
-        const userData: UserType = {
-          uid: data.uid,
-          name: data.name || null,
-          email: data.email || null,
-          image: data.image || null,
-        };
-        setUser({...userData});
-        return { success: true, user: userData };
-      } else {
-        return { success: false, msg: "No such document!" };
-      }
-    } catch (error: any) {
-      console.error("Error updating user data:", error);
-      return { success: false, msg: error.message };
-    }
-  };
-
-  const contextValue: AuthContextType = {
-    user,
-    setUser,
-    login,
-    register,
-    updateUserData,
-  };
-
-  return (
-    <AuthContext.Provider value={contextValue} >
-      {children}
-    </AuthContext.Provider>
-  );
+  
 };
 
 
