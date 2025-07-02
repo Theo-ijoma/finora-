81a1cf386b1822417c7d6a31272f145514a33dcd
Theodore Ijoma <ijomatheodore@gmail.com>
2025-07-02 17:17:34 +0100

    Auth Provider Error

--- constants/authContext.ts
+++ constants/authContext.ts
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useContext, useId, useState } from "react";
 import { auth, firestore } from "@/config/firebase";
 
 import {
@@ -8,79 +8,81 @@ import {
 import { doc, setDoc, getDoc } from "firebase/firestore";
 import { AuthContextType, UserType } from "@/type";
 
+
 const AuthContext = createContext<AuthContextType | null>(null);
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [user, setUser] = useState<UserType | null>(null);
+    const [user, setUser] = useState<UserType>(null);
 
-  const login = async (email: string, password: string) => {
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-      return { success: true };
-    } catch (error: any) {
-      return { success: false, msg: error.message };
-    }
-  };
+    const login = async (email: string, password: string) => {
+      try {
+        await signInWithEmailAndPassword(auth, email, password);
+        return { success: true, message: "Login successful" };
 
-  const register = async (email: string, password: string, name: string) => {
-    try {
-      const response = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-
-      await setDoc(doc(firestore, "users", response.user.uid), {
-        name,
-        email,
-        uid: response.user.uid,
-      });
-
-      return { success: true };
-    } catch (error: any) {
-      return { success: false, msg: error.message };
-    }
-  };
-
-  const updateUserData = async (uid: string) => {
-    try {
-      const docRef = doc(firestore, "users", uid);
-      const docSnap = await getDoc(docRef);
-
-      if (docSnap.exists()) {
-        const data = docSnap.data();
-        const userData: UserType = {
-          uid: data.uid,
-          name: data.name || null,
-          email: data.email || null,
-          image: data.image || null,
-        };
-        setUser({...userData});
-        return { success: true, user: userData };
-      } else {
-        return { success: false, msg: "No such document!" };
+      } catch (error: any) {
+        let msg = error.message;
+        return{success: false, message: msg};
       }
-    } catch (error: any) {
-      console.error("Error updating user data:", error);
-      return { success: false, msg: error.message };
     }
-  };
+
+    const register = async (email: string, password: string, name: string) => {
+      try {
+        let response = await createUserWithEmailAndPassword(auth, email, password);
+        await setDoc(doc(firestore, "users", response?.user?.uid), {
+          name,
+          email,
+          uid: response?.user?.uid,
+        });
+        return { success: true, message: "Registration successful" };
+      } catch (error: any) {
+        let msg = error.message;
+        return{success: false, message: msg};
+      }
+    };
+
+    const updateUserData = async (uid: string)=>{
+       try {
+        const docRef = doc(firestore, 'users', uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          const data = docSnap.data();
+          const userData: UserType ={
+            uid: data?.uid,
+            email: data.email || null,
+            name: data.name || null,
+            image: data.image || null,
+          };
+          setUser({...userData})
+        }
+      } catch (error: any) {
+        let msg = error.message;
+        // return{success: false, message: msg};
+        console.log('error', error);
+        
+      }
+    }
 
   const contextValue: AuthContextType = {
     user,
     setUser,
     login,
     register,
-    updateUserData,
-  };
+    updateUserData
+  }
 
-  return (
-    <AuthContext.Provider value={contextValue} >
-      {children}
-    </AuthContext.Provider>
-  );
+  return(
+    <AuthContext.Provider value={contextValue}> {children} </AuthContext.Provider>
+  )
 };
 
+
+export const useAuth =():AuthContextType =>{
+  const context = useContext(AuthContext);
+  if(!context){
+    throw new Error("useAuth must be wrapped inside AuthProvider")
+  }
+  return context;
+}
 
